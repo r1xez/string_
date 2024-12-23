0baeb9fef41865c7ac35d81af04d7921e70e7496
r1xez <164931719+r1xez@users.noreply.github.com>
2024-12-23 15:52:58 +0200

    comp

--- string_/main.cpp
+++ string_/main.cpp
@@ -1,8 +1,97 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
+template <typename T>
+class StringManipulator {
+private:
+    T str;
 
-	return 0;
+public:
+    StringManipulator(const T& input) : str(input) {}
+
+    void toUpperCase() {
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+    }
+
+    void toLowerCase() {
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+    }
+
+    void reverse() {
+        std::reverse(str.begin(), str.end());
+    }
+
+    void concatenate(const T& other) {
+        str += other;
+    }
+
+    T get() const {
+        return str;
+    }
+};
+
+template <>
+class StringManipulator<int> {
+private:
+    string str;
+
+public:
+    StringManipulator(int input) {
+        ostringstream oss;
+        oss << input;
+        str = oss.str();
+    }
+
+    void toUpperCase() {
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+    }
+
+    void toLowerCase() {
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+    }
+
+    void reverse() {
+        std::reverse(str.begin(), str.end());
+    }
+
+    void concatenate(const string& other) {
+        str += other;
+    }
+
+    string get() const {
+        return str;
+    }
+};
+
+int main() {
+    StringManipulator<string> stringManipulator("Hello, World!");
+
+    cout << "Original: " << stringManipulator.get() << endl;
+
+    stringManipulator.toUpperCase();
+    cout << "Uppercase: " << stringManipulator.get() << endl;
+
+    stringManipulator.toLowerCase();
+    cout << "Lowercase: " << stringManipulator.get() << endl;
+
+    stringManipulator.reverse();
+    cout << "Reversed: " << stringManipulator.get() << endl;
+
+    stringManipulator.concatenate(" How are you?");
+    cout << "Concatenated: " << stringManipulator.get() << endl;
+
+    StringManipulator<int> intManipulator(12345);
+    cout << "Original integer string: " << intManipulator.get() << endl;
+
+    intManipulator.reverse();
+    cout << "Reversed integer string: " << intManipulator.get() << endl;
+
+    intManipulator.concatenate("67890");
+    cout << "Concatenated integer string: " << intManipulator.get() << endl;
+
+    return 0;
 }
