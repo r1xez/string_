24802e62fc54a3c44fd212506f07634fcbc6d4bd
r1xez <164931719+r1xez@users.noreply.github.com>
2024-12-23 15:56:44 +0200

    comp

--- string_/main.cpp
+++ string_/main.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
+void readFile(const string& filename, vector<string>& data) {
+    ifstream file(filename);
+    string line;
 
-	return 0;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            data.push_back(line);
+        }
+        file.close();
+    }
+    else {
+        cout << "Unable to open file: " << filename << endl;
+    }
+}
+
+void writeFile(const string& filename, const vector<string>& data) {
+    ofstream file(filename);
+
+    if (file.is_open()) {
+        for (const auto& entry : data) {
+            file << entry << endl;
+        }
+        file.close();
+    }
+    else {
+        cout << "Unable to open file for writing: " << filename << endl;
+    }
+}
+
+int main() {
+    vector<string> surnames, names, patronymics;
+
+  
+    readFile("P.txt", surnames);
+    readFile("I.txt", names);
+    readFile("B.txt", patronymics);
+
+
+    if (surnames.size() != names.size() || surnames.size() != patronymics.size()) {
+        cout << "Error: The files have different numbers of records!" << endl;
+        return 1;
+    }
+
+    vector<string> fullNames;
+
+  
+    for (size_t i = 0; i < surnames.size(); ++i) {
+        fullNames.push_back(surnames[i] + " " + names[i] + " " + patronymics[i]);
+    }
+
+    sort(fullNames.begin(), fullNames.end());
+
+    
+    writeFile("Report.txt", fullNames);
+
+    cout << "Operation completed. Results saved to Report.txt." << endl;
+
+    return 0;
 }
